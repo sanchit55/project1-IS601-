b3d42797e87f28371b8f428bfa68fc8dfc494941
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-10-22 14:30:13 -0400

    final

--- index.php
+++ index.php
@@ -3,23 +3,23 @@
 class Assembly{
 	public static function autoload($class)
 	{
-		include $class . '.php';
+		include $class . '.php'; // Specific Files to be loaded
 	}
 }
-spl_autoload_register(array('Assembly','autoload'));
+spl_autoload_register(array('Assembly','autoload')); //To create a queue of autoload function
 $obj = new main();
 
 class main{
 	public function __construct()
 	{
 		$pageCall='formlayout';
-		if(isset($_REQUEST['sheet']))
+		if(isset($_REQUEST['sheet'])) // Requsting variables from the sheet
 		{
-			$pageCall = $_REQUEST['sheet'];
+			$pageCall = $_REQUEST['sheet'];   
 		}
 
 		$sheet = new $pageCall;
-		if($_SERVER['REQUEST_METHOD'] == 'GET')
+		if($_SERVER['REQUEST_METHOD'] == 'GET') //Contans server and execution envoirnment information 
 		{
 			$sheet->get();
 		}
