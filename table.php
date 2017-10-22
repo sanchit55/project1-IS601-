cb845a55b9a67374c14e4ebf8ba20d3636dd1f58
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-10-22 12:54:13 -0400

    pushing table

---
+++ table.php
@@ -0,0 +1,57 @@
+<?php
+
+class table extends sheet{
+
+	public function get()
+    {
+    	$row=1;
+    	if (($handle=fopen($_GET['filename'], "r")) !==false)
+    	{
+    		echo '<table border="1">';
+    		while(($info=fgetcsv($handle,1500,",")) !==false)
+    		{
+    			$num=count($info);
+    			if ($row == 1)
+    			{
+    				echo'<thead><tr>';
+    			}
+    			else
+    			{
+    				echo '<tr>';
+    			}
+
+    			for ($c=0; $c<$num; $c++)
+    			{
+    				if(empty($info[$c]))
+    				{
+    					$output = "&nbsp;";
+    				}
+    				else
+    				{
+    					$output = $info[$c];
+    				}
+    				if ($row ==1)
+    				{
+    					echo '<th>'.$output.'</th>';
+    				}
+    				else
+    				{
+    					echo '<td>'.$output.'</td>';
+    				}
+    			}
+    			    
+    			    if ($row == 1)
+    			    {
+    				    echo '</tr></thead><tbody>';
+    			    }
+    			    else
+    			    {
+    				    echo '</tr>';
+    			    }
+                    $row++;
+    		    }
+    		        echo '</tbody></table>';
+    	            fclose($handle);
+    	    }
+        }
+    }
