06882ce8dc835ac404bc87fba08dbaf475b22aff
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-10-22 13:27:28 -0400

    second

--- formlayout.php
+++ formlayout.php
@@ -7,7 +7,7 @@ class formlayout extends sheet
  public function get()
  {
  	
- 	$view = '<form method="post" enctype="multipart/form-data">';
+ 	$view = '<form method="post" enctype="multipart/form-data"><hr><br>';
  	$view.= '<input type="file" name="filesToUpload" id="filesToUpload"><br>';
  	$view.= '<input type="submit" value="Upload File" name="submit">';
  	$view.= '</form>';
@@ -20,9 +20,6 @@ public function post()
  {
 	$target_dir="uploads/";
 	$target_file=$target_dir.basename($_FILES["filesToUpload"]["name"]);
-
-	$imageFileType=pathinfo($target_file,PATHINFO_EXTENSION);
-	$imageFIleName=pathinfo($target_file,PATHINFO_EXTENSION);
 
 	move_uploaded_file($_FILES["filesToUpload"]["tmp_name"],$target_file);
 	header('Location:index.php?sheet=table&filename='.$target_file);
